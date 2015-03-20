61040c682d5ec9b2e86d9223b0ea05b03e35224e
Jaap Jansma <jaap@edeveloper.nl>
2015-03-20 11:21:48 +0100

    completed tokens for kwartaal bijdrage

--- CRM/Kwartaalbijdrage/Tokens.php
+++ CRM/Kwartaalbijdrage/Tokens.php
@@ -7,6 +7,8 @@ class CRM_Kwartaalbijdrage_Tokens {
     public static function tokens(&$tokens) {
         $tokens['kwartaalbijdrage']['kwartaalbijdrage.vorig_kwartaal_nr'] = 'Vorig kwartaal nr';
         $tokens['kwartaalbijdrage']['kwartaalbijdrage.vorig_kwartaal'] = 'Kwartaalbijdrage vorig kwartaal';
+        $tokens['kwartaalbijdrage']['kwartaalbijdrage.huidig_kwartaal_nr'] = 'Huidig kwartaal nr';
+        $tokens['kwartaalbijdrage']['kwartaalbijdrage.huidig_kwartaal'] = 'Kwartaalbijdrage huidig kwartaal';
     }
 
     /**
@@ -28,11 +30,28 @@ class CRM_Kwartaalbijdrage_Tokens {
             if (in_array('vorig_kwartaal', $tokens['kwartaalbijdrage']) || array_key_exists('vorig_kwartaal', $tokens['kwartaalbijdrage'])) {
                 $this->vorig_kwartaal($values, $cids, $job, $tokens, $context);
             }
+            if (in_array('huidig_kwartaal_nr', $tokens['kwartaalbijdrage']) || array_key_exists('huidig_kwartaal_nr', $tokens['kwartaalbijdrage'])) {
+                $this->huidig_kwartaal_nr($values, $cids, $job, $tokens, $context);
+            }
+            if (in_array('huidig_kwartaal', $tokens['kwartaalbijdrage']) || array_key_exists('huidig_kwartaal', $tokens['kwartaalbijdrage'])) {
+                $this->huidig_kwartaal($values, $cids, $job, $tokens, $context);
+            }
+        }
+    }
+
+    protected function huidig_kwartaal_nr(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+        $curQuarter = $this->getQuarter();
+        foreach($cids as $cid) {
+            $values[$cid]['kwartaalbijdrage.huidig_kwartaal_nr'] = $curQuarter;
         }
     }
 
     protected function vorig_kwartaal_nr(&$values, $cids, $job = null, $tokens = array(), $context = null) {
         $curQuarter = $this->getQuarter();
+        $curQuarter = $curQuarter - 1;
+        if ($curQuarter < 1) {
+            $curQuarter = 4;
+        }
         foreach($cids as $cid) {
             $values[$cid]['kwartaalbijdrage.vorig_kwartaal_nr'] = $curQuarter;
         }
@@ -63,13 +82,47 @@ class CRM_Kwartaalbijdrage_Tokens {
                 break;
         }
 
-        //$startDate->modify('-3 month'); //previous quarter
+        $startDate->modify('-3 month'); //previous quarter
         $endDate = clone $startDate;
         $endDate->modify('+3 month');
         $endDate->modify('-1 day');
 
         foreach($cids as $cid) {
             $values[$cid]['kwartaalbijdrage.vorig_kwartaal'] = $this->formatTotal($cid, $startDate, $endDate);
+        }
+    }
+
+    protected function huidig_kwartaal(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+        $startDate = new DateTime();
+        switch($startDate->format('m')) {
+            case 1:
+            case 2:
+            case 3:
+                $startDate->setDate($startDate->format('Y'), 1, 1); //first day of current month;
+                break;
+            case 4:
+            case 5:
+            case 6:
+                $startDate->setDate($startDate->format('Y'), 4, 1); //first day of current month;
+                break;
+            case 7:
+            case 8:
+            case 9:
+                $startDate->setDate($startDate->format('Y'), 7, 1); //first day of current month;
+                break;
+            case 10:
+            case 11:
+            case 12:
+                $startDate->setDate($startDate->format('Y'), 10, 1); //first day of current month;
+                break;
+        }
+
+        $endDate = clone $startDate;
+        $endDate->modify('+3 month');
+        $endDate->modify('-1 day');
+
+        foreach($cids as $cid) {
+            $values[$cid]['kwartaalbijdrage.huidig_kwartaal'] = $this->formatTotal($cid, $startDate, $endDate);
         }
     }
 
@@ -110,7 +163,9 @@ class CRM_Kwartaalbijdrage_Tokens {
 
         $return = "<style type=\"text/css\">
                     table.token_kwartaalbijdrage { border: none; width: 100%; }
-                    table.token_kwartaalbijdrage .sub_total { font-weight: bold }
+                    table.token_kwartaalbijdrage .sub_total { font-weight: bold; text-align: right; }
+                    table.token_kwartaalbijdrage .text-right { text-align: right; }
+                    table.token_kwartaalbijdrage .border-top { border-top: 1px solid black; }
                     </style>
                     <table class=\"token_kwartaalbijdrage\">";
         $return .= "<tr><td>I</td><td>Basisbedrag</td><td></td><td></td><td></td></tr>";
@@ -120,6 +175,39 @@ class CRM_Kwartaalbijdrage_Tokens {
             $return .= "<tr><td></td><td>".$month[(int) $data->date->format('m')]."</td><td></td><td>".CRM_Utils_Money::format($data->basisbedrag, 'EUR')."</td><td></td>";
         }
         $return .= "<tr><td></td><td></td><td></td><td></td><td class=\"sub_total\">".CRM_Utils_Money::format($totaal_basis, 'EUR')."</td></tr>";
+
+        $return .= "<tr><td>II</td><td>Ledenvergoeding</td><td></td><td></td><td></td></tr>";
+        $return .= "<tr><td></td><td></td><td class=\"text-right\">Aantal</td><td></td><td></td></tr>";
+        $totaal_leden = 0;
+        $leden_vergoeding = 0.00;
+        foreach($datas as $data) {
+            $totaal_leden += $data->aantal_leden;
+            $leden_vergoeding += $data->ledenvergoeding;
+            $return .= "<tr><td></td><td>".$month[(int) $data->date->format('m')]."</td><td class=\"text-right\">".$data->aantal_leden."</td><td></td><td></td></tr>";
+        }
+        $leden_vergoeding_per_lid = $leden_vergoeding / $totaal_leden;
+        $return .= "<tr><td></td><td>Totaal</td><td class=\"text-right border-top\">".$totaal_leden."</td><td>x ".CRM_Utils_Money::format($leden_vergoeding_per_lid, 'EUR')."</td><td class=\"sub_total\">".CRM_Utils_Money::format($leden_vergoeding, 'EUR')."</td></tr>";
+
+        $return .= "<tr><td>III</td><td>Tribunebezorging</td><td></td><td></td><td></td></tr>";
+        $return .= "<tr><td></td><td></td><td class=\"text-right\">Aantal</td><td></td><td></td></tr>";
+        $totaal_tribunes = 0;
+        $tribune_vergoeding = 0.00;
+        foreach($datas as $data) {
+            $totaal_tribunes += $data->bezorgde_tribunes;
+            $tribune_vergoeding += $data->tribunebezorging_vergoeding;
+            $return .= "<tr><td></td><td>".$month[(int) $data->date->format('m')]."</td><td class=\"text-right\">".$data->bezorgde_tribunes."</td><td></td><td></td></tr>";
+        }
+        $return .= "<tr><td></td><td>Sub totaal</td><td class=\"text-right border-top\">".$totaal_tribunes."</td><td></td><td></td></tr>";
+        $return .= "<tr><td></td><td>Waarvan vergoed via de ledenvergeoding</td><td class=\"text-right border-top\">".$totaal_leden."</td><td></td><td></td></tr>";
+        $tribune_vergoeding_vergoeding_tribune = $tribune_vergoeding / ($totaal_tribunes - $totaal_leden);
+        $return .= "<tr><td></td><td>Totaal</td><td class=\"text-right border-top\">".($totaal_tribunes - $totaal_leden)."</td><td>x ".CRM_Utils_Money::format($tribune_vergoeding_vergoeding_tribune, 'EUR')."</td><td class=\"sub_total\">".CRM_Utils_Money::format($tribune_vergoeding, 'EUR')."</td></tr>";
+
+        $totale_vergoeding = 0.00;
+        foreach($datas as $data) {
+            $totale_vergoeding += $data->totaal_bijdrage;
+        }
+        $return .= "<tr><td></td><td colspan='2'><strong>Kwartaalbijdrage</strong></td><td colspan='2' class=\"text-right border-top\"><strong>".CRM_Utils_Money::format($totale_vergoeding, 'EUR')."</trong></td></tr>";
+
         $return .= "</table>";
 
         return $return;
@@ -130,10 +218,6 @@ class CRM_Kwartaalbijdrage_Tokens {
         $now = new DateTime();
         $curMonth = date("m", time());
         $curQuarter = ceil($curMonth/3);
-        $curQuarter - 1;
-        if ($curQuarter < 1) {
-            $curQuarter = 4;
-        }
         return $curQuarter;
     }
 
